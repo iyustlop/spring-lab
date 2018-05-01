ac392e63c57f3031f41b7e3b767aeec480aec18a
iyustlop <iyustlop@gmail.com>
2018-05-01 17:13:38 +0200

    Porsche Initial commit

---
+++ spring-porsche/src/main/java/com/freelance/spring/springporsche/controller/PorscheController.java
@@ -0,0 +1,18 @@
+package com.freelance.spring.springporsche.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PorscheController {
+@Value("${models}") String models;
+	
+	@RequestMapping("/")
+	  public @ResponseBody String getHeroe() {
+	    String[] modelsArray = models.split(",");
+	    int i = (int)Math.round(Math.random() * (modelsArray.length - 1));
+	    return modelsArray[i];
+	  }
+}
